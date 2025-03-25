bf644dd9de6feb533c357e776c8c9ae3c94072c7
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2025-03-26 00:36:00 +0700

    feat: add converters and refactor employee management logic
    - Introduced `DateTimeToDateTimeOffsetConverter` and `TimeToStringConverter` for data conversions.
    - Enhanced `EmployeeViewModel` with a new `SelectedUser` property and improved pagination logic.
    - Implemented `TurnOnEditing` command for editing employee details.
    - Added a popup for viewing and editing employee details.

---
+++ Converters/DateTimeToDateTimeOffsetConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace kafi.Converters
+{
+    public class DateTimeToDateTimeOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTime dateTime)
+            {
+                return new DateTimeOffset(dateTime);
+            }
+            return DateTimeOffset.MinValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTimeOffset dateTimeOffset)
+            {
+                return dateTimeOffset.DateTime;
+            }
+            return DateTime.MinValue;
+        }
+    }
+}
