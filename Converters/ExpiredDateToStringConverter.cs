0dedce05e841cd6d7352bcbcd40aa240a5ff4b43
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2025-03-25 15:21:51 +0700

    feat: add DataGrid styles and new converters for improving inventory view

---
+++ Converters/ExpiredDateToStringConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace kafi.Converters
+{
+    public class ExpiredDateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not DateTime expiryDate)
+                return "";
+
+            DateTime now = DateTime.Now;
+            // Nếu đã hết hạn
+            if (expiryDate < now)
+                return "Expired";
+
+            // Tính số tháng chênh lệch giữa expiryDate và now
+            int monthsDiff = (expiryDate.Year - now.Year) * 12 + expiryDate.Month - now.Month;
+
+            if (monthsDiff == 3)
+            {
+                return "Còn 3 tháng";
+            }
+            else if (monthsDiff == 2)
+            {
+                return "Còn 2 tháng";
+            }
+            else if (monthsDiff == 1)
+            {
+                // Tính số ngày còn lại, làm tròn lên
+                int days = (int)Math.Ceiling((expiryDate - now).TotalDays);
+                return $"Còn {days} ngày";
+            }
+            else
+            {
+                // Hiển thị ngày hết hạn theo định dạng dd/MM/yyyy (không có thời gian)
+                return expiryDate.ToString("dd/MM/yyyy");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Converters/StatusToForegroundConverter.cs
+++ Converters/StatusToForegroundConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using kafi.Models;
 using Microsoft.UI;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
@@ -7,19 +6,31 @@ using Windows.UI;
 
 namespace kafi.Converters
 {
-    class PaymentTypeToForegroundConverter : IValueConverter
+    class StatusToForegroundConverter : IValueConverter
     {
-
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+            if (value is int currentStock)
+            {
+                value = currentStock switch
+                {
+                    > 0 => "InStock",
+                    < 20 => "OutOfStock",
+                };
+            }
+
             switch (value)
             {
-                case "Cash":
+                case "Cash" or "InStock":
                     return new SolidColorBrush(Color.FromArgb(255, 69, 131, 83));
                 case "QR":
                     return new SolidColorBrush(Color.FromArgb(255, 255, 154, 0));
+                case "OutOfStock":
+                    return new SolidColorBrush(Color.FromArgb(255, 197, 44, 164));
+                default:
+                    break;
             }
-            
+
             return new SolidColorBrush(Colors.Transparent);
         }
         public object ConvertBack(object value, Type targetType, object parameter, string language)
