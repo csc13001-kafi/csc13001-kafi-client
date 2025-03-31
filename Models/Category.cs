f4b9537e41f93d1514d6581e6ffbbe1255dfa5d0
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2025-03-31 16:10:40 +0700

    feat: add ICategoryDao and IProductDao implementations for category and product management

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,23 +1,35 @@
 ﻿using System;
+using System.IO;
 using System.Text.Json.Serialization;
 
 namespace kafi.Models
 {
+    public record CreateCategoryRequest
+    (
+        string Name,
+        Stream FileStream,
+        string ContentType,
+        string FileName
+    );
+
     public class Category
     {
         [JsonPropertyName("id")]
-        public string Id { get; set; }
-        
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Guid Id { get; set; }
+
         [JsonPropertyName("name")]
         public string Name { get; set; }
-        
+
         [JsonPropertyName("image")]
         public string Image { get; set; }
-        
+
         [JsonPropertyName("createdAt")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public DateTime CreatedAt { get; set; }
-        
+
         [JsonPropertyName("updatedAt")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public DateTime UpdatedAt { get; set; }
     }
 }
--- Repositories/MenuRepository.cs
+++ Repositories/MenuRepository.cs
@@ -1,52 +1,121 @@
-using kafi.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using kafi.Models;
+using kafi.Models.Inventory;
 
 namespace kafi.Data
 {
     public interface IMenuRepository
     {
-        Task<IEnumerable<Product>> GetAllMenuItems();
-        Task<Product?> GetMenuItemById(string id);
-        Task<IEnumerable<Category>> GetAllCategories();
         Task<CategoryProductsResponse> GetCategoriesAndProducts();
-        Task<IEnumerable<Product>> GetMenuItemsByCategory(int categoryId);
+        Task<IEnumerable<Inventory>> GetMaterials();
+
+        Task<Product> AddProduct(CreateProductRequest request);
+        Task UpdateProduct(Guid id, CreateProductRequest request);
+        Task DeleteProduct(Guid id);
+
+        Task<Category> AddCategory(CreateCategoryRequest request);
+        Task UpdateCategory(Guid id, CreateCategoryRequest request);
+        Task DeleteCategory(Guid id);
     }
 
-    public class MenuRepository : IMenuRepository
+    public class MenuRepository(IMenuDao menuDao, ICategoryDao categoryDao, IProductDao productDao, IInventoryDao inventoryDao) : IMenuRepository
     {
-        private readonly IMenuDao _menuDao;
+        private readonly IMenuDao _menuDao = menuDao;
+        private readonly ICategoryDao _categoryDao = categoryDao;
+        private readonly IProductDao _productDao = productDao;
+        private readonly IInventoryDao _inventoryDao = inventoryDao;
 
-        public MenuRepository(IMenuDao menuDao)
+        public async Task<Category> AddCategory(CreateCategoryRequest request)
         {
-            _menuDao = menuDao;
+            using var form = new MultipartFormDataContent();
+            var fileContent = new StreamContent(request.FileStream);
+            fileContent.Headers.Add("Content-Type", request.ContentType);
+
+            form.Add(fileContent, "file", request.FileName);
+            form.Add(new StringContent(request.Name), "name");
+
+            return (Category)await _categoryDao.Add(form);
+        }
+
+        public async Task<Product> AddProduct(CreateProductRequest request)
+        {
+            using var form = new MultipartFormDataContent();
+            var fileContent = new StreamContent(request.FileStream);
+            fileContent.Headers.Add("Content-Type", request.ContentType);
+
+            form.Add(fileContent, "file", request.FileName);
+            form.Add(new StringContent(request.Name), "name");
+            form.Add(new StringContent(request.Price.ToString()), "price");
+            form.Add(new StringContent(request.IsAvailable.ToString()), "onStock");
+            form.Add(new StringContent(request.CategoryId.ToString()), "categoryId");
+
+            var materialIds = string.Join(",", request.Materials.Select(m => m.Id.ToString()));
+            var quantities = string.Join(",", request.Materials.Select(m => m.Quantity.ToString()));
+
+            form.Add(new StringContent(materialIds), "materials");
+            form.Add(new StringContent(quantities), "quantity");
+            return (Product)await _productDao.Add(form);
+        }
+
+        public async Task DeleteCategory(Guid id)
+        {
+            await _categoryDao.Delete(id);
+        }
+
+        public async Task DeleteProduct(Guid id)
+        {
+            await _productDao.Delete(id);
         }
 
         public async Task<CategoryProductsResponse> GetCategoriesAndProducts()
         {
-            return await _menuDao.GetCategoriesAndProducts();
-        }
-
-        public async Task<IEnumerable<Product>> GetAllMenuItems()
-        {
-            return await _menuDao.GetAll();
-        }
-
-        public async Task<Product?> GetMenuItemById(string id)
-        {
-            return await _menuDao.GetById(id);
-        }
-
-        public async Task<IEnumerable<Category>> GetAllCategories()
-        {
             var response = await _menuDao.GetCategoriesAndProducts();
-            return response.Categories;
+            var products = await _productDao.GetAll();
+            response.Products = [.. products];
+            return response;
         }
 
-        public async Task<IEnumerable<Product>> GetMenuItemsByCategory(int categoryId)
+        public async Task<IEnumerable<Inventory>> GetMaterials()
         {
-            throw new NotImplementedException();
+            return await _inventoryDao.GetAll();
+        }
+
+        public async Task UpdateCategory(Guid id, CreateCategoryRequest request)
+        {
+            using var form = new MultipartFormDataContent();
+            if (request.FileStream != null)
+            {
+                var fileContent = new StreamContent(request.FileStream);
+                fileContent.Headers.Add("Content-Type", request.ContentType);
+                form.Add(fileContent, "file", request.FileName);
+            }
+            form.Add(new StringContent(request.Name), "name");
+
+            await _categoryDao.Update(id, form);
+        }
+
+        public async Task UpdateProduct(Guid id, CreateProductRequest request)
+        {
+            using var form = new MultipartFormDataContent();
+            if (request.FileStream != null)
+            {
+                var fileContent = new StreamContent(request.FileStream);
+                fileContent.Headers.Add("Content-Type", request.ContentType);
+                form.Add(fileContent, "file", request.FileName);
+            }
+            form.Add(new StringContent(request.Name), "name");
+            form.Add(new StringContent(request.Price.ToString()), "price");
+            form.Add(new StringContent(request.IsAvailable.ToString()), "onStock");
+            form.Add(new StringContent(request.CategoryId.ToString()), "categoryId");
+            var materialIds = string.Join(",", request.Materials.Select(m => m.Id.ToString()));
+            var quantities = string.Join(",", request.Materials.Select(m => m.Quantity.ToString()));
+            form.Add(new StringContent(materialIds), "materials");
+            form.Add(new StringContent(quantities), "quantity");
+            await _productDao.Update(id, form);
         }
     }
 }
